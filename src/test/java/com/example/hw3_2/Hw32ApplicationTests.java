fc716dacbfc45dd4553d55f35ed99700129f829c
Dianakyz <3222777my@gmail.com>
2022-12-13 18:11:46 +0300

    first commit

---
+++ src/test/java/com/example/hw3_2/Hw32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
